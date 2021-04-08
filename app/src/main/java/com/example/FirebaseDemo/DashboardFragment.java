3061f820748e991449058d52c32ef2c57c894e3a
student <manojgoud554@gmail.com>
2021-04-07 23:38:09 -0400

    Updated project with profile and firestore

---
+++ app/src/main/java/com/example/FirebaseDemo/DashboardFragment.java
@@ -0,0 +1,115 @@
+package com.example.FirebaseDemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+public class DashboardFragment extends Fragment {
+
+
+    Button btn_signOut;
+    FirebaseUser user;
+    FirebaseFirestore fireStore;
+    NavController navController;
+    TextView txt_Name;
+
+    public DashboardFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        user = getArguments().getParcelable("user");
+        fireStore = FirebaseFirestore.getInstance();
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setHasOptionsMenu(true);
+        btn_signOut = view.findViewById(R.id.btn_signOut);
+        txt_Name = view.findViewById(R.id.txt_welcome);
+
+        navController = Navigation.findNavController(getActivity(), R.id.host_fragment);
+
+        readFireStore();
+
+        btn_signOut.setOnClickListener(view1 -> {
+
+            FirebaseAuth.getInstance().signOut();
+            navController.navigate(R.id.loginFragment);
+
+        });
+    }
+
+    public void readFireStore()
+    {
+        DocumentReference docRef = fireStore.collection("User").document(user.getUid());
+
+        docRef.get().addOnCompleteListener(task -> {
+             if (task.isSuccessful())
+             {
+                 DocumentSnapshot doc = task.getResult();
+
+                 if (doc.exists())
+                 {
+                     Log.d("DashboardFragment",doc.getData().toString());
+
+                     txt_Name.setText("Welcome "+doc.get("Name") + " !");
+
+
+                 }
+
+             }
+        });
+    }
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        inflater.inflate(R.menu.navigation,  menu);
+        super.onCreateOptionsMenu(menu, inflater);
+
+    }
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        switch (menuItem.getItemId()){
+            case R.id.my_profile:
+                startActivity(new Intent(getContext(),profile.class));
+                getActivity().finish();
+                return true;
+            default:break;
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/FirebaseDemo/profile.java
@@ -0,0 +1,16 @@
+package com.example.FirebaseDemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.MyApplication.R;
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
